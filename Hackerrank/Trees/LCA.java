03a63484e5648b42b50214fc1f284f35c2158b05
Saket Ringsia <ringsiasaket@gmail.com>
2019-07-28 12:06:09 +0530

    Create LCA.java

---
+++ Hackerrank/Trees/LCA.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+
+	/*
+    class Node 
+    	int data;
+    	Node left;
+    	Node right;
+	*/
+	public static Node lca(Node root, int v1, int v2) {
+      	// Write your code here.
+        if(root==null)
+            return null;
+        if(v1 < root.data && v2 < root.data)
+            return lca(root.left,v1,v2);
+        if(v1 > root.data && v2 > root.data)
+            return lca(root.right,v1,v2);
+        
+        return root;
+    }
+	public static Node insert(Node root, int data) {
